4357c3b32f87fa948923743f24f9d8c38704c911
myronferz2309 <myronfernandes239@gmail.com>
2026-08-20 10:48:17 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins + Maven!");
+        System.out.println("Java version: 21");
+    }
+}
