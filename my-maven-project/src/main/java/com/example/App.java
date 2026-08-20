d696122c1cb84453a0a0b1233b1fd75ca66f44d0
myronferz2309 <myronfernandes239@gmail.com>
2026-08-20 10:42:12 +0530

    Update App.java

--- my-maven-project/src/main/java/com/example/App.java
+++ my-maven-project/src/main/java/com/example/App.java
@@ -1,9 +1,1 @@
-package com.example;
 
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + Maven!");
-        System.out.println("Java version: 21");
-    }
-}
